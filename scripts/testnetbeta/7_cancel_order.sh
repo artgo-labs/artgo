6d9b07e9f45bff68cf44184e2f646c17374639bc
MichaelWang <michael_wangguanyi@163.com>
2024-06-19 11:59:03 +0800

    script: add cancel_orader

--- scripts/testnetbeta/7_cancel_order.sh
+++ scripts/testnetbeta/7_cancel_order.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-misc-toolkit execute \
---private-key APrivateKey1zkpGze7wAkvb3XQdNmnFyN9NSC1NiGGMU3LoRw9yH17Yb1J \
---endpoint http://hk2.s.filfox.io:60050 \
--b \
-artgo_aigc_v1_6.aleo cancel_order "{
+snarkos developer execute \
+--private-key "$PK_2" \
+--query http://hk2.s.filfox.io:60053 \
+-b http://hk2.s.filfox.io:60053/testnet/transaction/broadcast \
+--network 1 \
+artgo_aigc_v0_1.aleo cancel_order "{
                                      owner: aleo1rh4whtqef93v2et8j9lujxr67l64fz9ctgn035lwqmp9h6wdf5psa4uu88.private,
-                                     microcredits: 1000000u64.private,
-                                     _nonce: 8381326883085761753591694771733808260640201675470744349415443474755472590644group.public
-                                   }" 1000000u64 2u128 aleo1tjw6lw3zgk4q4vgmwctzerr9tgyay8v0rn3asp7rwujtcj0lwsrq4mr9ng
+                                     microcredits: 2000000u64.private,
+                                     _nonce: 304019483221054690564021711033944453702373355697032097780839821942351474209group.public
+                                   }" 1000000u64 1u128 "$ADDR_2"
+
+# tx hash: at1wns2zf79jav5wdstmgl76gx59yr0k3sldmwnynjn0dyc6mq63qqqcnr48z
